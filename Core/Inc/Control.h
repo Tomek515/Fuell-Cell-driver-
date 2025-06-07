b0359e894169d0e2f5355aca19e37cc724330f27
Tomek515 <149333002+Tomek515@users.noreply.github.com>
2025-06-07 10:05:08 +0200

    blowers

--- Core/Inc/Control.h
+++ Core/Inc/Control.h
@@ -22,7 +22,7 @@ extern TIM_HandleTypeDef htim1;
 extern float t;
 //extern TIM_HandleTypeDef htim1;
 
-#define CAPACITOR_LOW_LEVEL 25.0f
+#define CAPACITOR_LOW_LEVEL 30.0f
 #define CAPACITOR_HIGH_LEVEL 42.5f
 #define MAX_SAFE_TEMP      60.0f
 #define MAX_SAFE_POWER     1000.0f
